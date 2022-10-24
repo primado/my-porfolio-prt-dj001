7a05d8590ac1e49b2d55c423e9c9bfb88ad9a042
primado <primadoprincepa@gmail.com>
2022-10-24 08:37:39 +0000

    setting up deployment configurations

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio-db',
+        'USER': 'primado@portfolio-prt-db',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': 'portfolio-prt-db.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': "require",
+        }
     }
 }
 
