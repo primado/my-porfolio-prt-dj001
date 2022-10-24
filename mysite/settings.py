13f80f8c42a212e6ca3b065dafe2a8db12e784e0
primado <primadoprincepa@gmail.com>
2022-10-24 07:39:45 +0000

    setting up deployment configurations

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['https://prince-adimado-prt.azurewebsites.net']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
