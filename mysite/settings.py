df86da4a694e3cf523176ea021181b587bd071a9
primado <primadoprincepa@gmail.com>
2022-10-25 09:17:50 +0000

    added lists to ALLOWED HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://prince-adimado-prt.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://prince-adimado-prt.azurewebsites.net']
 
 
 # Application definition
